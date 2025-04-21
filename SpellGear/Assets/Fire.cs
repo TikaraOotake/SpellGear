b67f8ac49f3045113a4f723d8b77e66a00c303ed
731PC-12\student <741PC>
2025-04-21 10:47:59 +0900

    もっかい

--- SpellGear/Assets/Fire.cs
+++ SpellGear/Assets/Fire.cs
@@ -10,12 +10,12 @@ public class Fire : MonoBehaviour
     float DestroyTime = 1.0f;
     void Start()
     {
-        
+
     }
 
     private void Update()
     {
-        transform.localScale += new Vector3(1.0f, 1.0f, 1.0f) * Time.deltaTime;
+        transform.localScale += new Vector3(1.0f, 1.0f, 1.0f) * Time.deltaTime * 2.0f;
 
         DestroyTimer += Time.deltaTime;
         if (DestroyTimer >= DestroyTime)
