b9fe8123210fc9613b78ba7387782602c3d4cf62
student <>
2025-04-22 18:44:09 +0900

    テスト用のScriptを作成

---
+++ SpellGear/Assets/Script/Player/TestPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class TestPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+        }
+    }
+}
