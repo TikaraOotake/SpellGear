5c2a100e6e63a44b9791707ca49cc60188c3e468
student <>
2025-05-01 16:43:27 +0900

    0501更新

--- SpellGear/Assets/Script/Player/TestPlayer.cs
+++ SpellGear/Assets/Script/Player/TestPlayer.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter))]
-[RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(Rigidbody))]
 
 public class TestPlayer : MonoBehaviour
